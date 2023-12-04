022e7b8db45c3d1ea560639607a2c43b904d2075
lansen <lansen_i@didiglobal.com>
2023-12-04 11:17:10 +0800

    feat: 移除 Foo 示例组件

--- src/Foo/index.md
+++
@@ -1,9 +0,0 @@
-# Foo
-
-This is an example component.
-
-```jsx
-import { Foo } from 'cobalt-ui';
-
-export default () => <Foo title="Hello dumi!" />
-```
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import Button from './Button';
-import Foo from './Foo';
 import GlobalConfig from './GlobalConfig';
 
-export { Button, Foo, GlobalConfig };
+export { Button, GlobalConfig };
