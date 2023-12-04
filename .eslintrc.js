e7dd7285d1d1e466992d31797318bebcf3de0bcf
lansen <lansen_i@didiglobal.com>
2023-12-04 11:05:14 +0800

    feat: 修改 eslint 和 prettier 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: require.resolve('@umijs/lint/dist/config/eslint'),
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-console': 'warn',
+  },
 };
