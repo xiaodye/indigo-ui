7b950b5d459803288f2efa98a9f777b3932e1f6c
lansen <bluewoods.lan@gmail.com>
2023-12-29 23:19:05 +0800

    feat: npm publish

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 npm config get registry # 检查仓库镜像库
-npm config set registry=http://registry.npmjs.org
+npm config set registry=https://registry.npmjs.org # 切换 npm 官方镜像
 echo '请进行登录相关操作：'
 npm login # 登陆
 echo "-------publishing-------"
 npm publish # 发布
-npm config set registry=https://registry.npm.taobao.org # 设置为淘宝镜像
+npm config set registry=https://registry.npmmirror.com # 设置为淘宝镜像
 echo "发布完成"
 exit
